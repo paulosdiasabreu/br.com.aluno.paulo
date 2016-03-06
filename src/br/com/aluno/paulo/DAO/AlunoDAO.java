022e857e978b4923cddc91c8b88bd67d68a02e05
paulosdiasabreu <paulosdiasabreu@gmail.com>
2016-03-06 15:03:52 -0300

    Implementação JSF
    Inicio da implementação das telas de formulário.

--- src/br/com/aluno/paulo/DAO/AlunoDAO.java
+++ src/br/com/aluno/paulo/DAO/AlunoDAO.java
@@ -1,49 +1,15 @@
 package br.com.aluno.paulo.DAO;
 
-import java.util.List;
-
-import javax.persistence.*;
+//import java.util.List;
 import br.com.aluno.paulo.entidade.*;
+//import javax.persistence.*;
+import javax.ejb.*;
 
-public class AlunoDAO {
+@Stateless
+public class AlunoDAO extends GenericoDAO<Aluno>{
 	
-	private EntityManager em;
-	
-	public String salvarAluno(Aluno aluno){
-		
-		try{
-			em.merge(aluno);
-			//merge realiza o comando de insert ou update
-		}catch(Exception ex){
-			return "Ocorreu o erro"+ex.getMessage();
-		}
-		
-		return null;
-	}
-	
-	public Aluno consultarAluno(Integer matricula){
-		Aluno a = em.find(Aluno.class, matricula);
-		
-		return a;
-	}
-	
-	public List<Aluno> todosAlunos(){
-		TypedQuery<Aluno> query = em.createQuery("select a from Aluno as a", Aluno.class);
-		
-		return query.getResultList();
-	}
-	
-	public String excluirAluno(Integer matricula){
-		try{
-			
-			Aluno aluno = consultarAluno(matricula);
-			em.remove(aluno);
-			
-		}catch(Exception ex){
-			return "Erro ao excluir: "+ex.getMessage();
-		}
-		
-		return null;
+	public AlunoDAO(){
+		super(Aluno.class);
 	}
 	
 	
--- src/br/com/aluno/paulo/DAO/BoletoDAO.java
+++ src/br/com/aluno/paulo/DAO/BoletoDAO.java
@@ -1,7 +1,9 @@
 package br.com.aluno.paulo.DAO;
 
 import br.com.aluno.paulo.entidade.*;
+import javax.ejb.*;
 
+@Stateless
 public class BoletoDAO extends GenericoDAO<Boleto> {
 
 	public BoletoDAO(){
